30b767bf9a1e2c0d7cdeab5bebb36c8cf30baa36
Carlos Acevedo <cacevedod@devco.com.co>
2023-06-20 19:15:19 -0500

    cobertura, cov checks, task ci y headless

--- karma.conf.js
+++ karma.conf.js
@@ -28,9 +28,18 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, './coverage/unit-test-angular'),
       subdir: '.',
       reporters: [
-        { type: 'html' },
+        { type: 'lcov' },
+        { type: 'cobertura' },
         { type: 'text-summary' }
-      ]
+      ],
+      check: {
+        global: {
+          statements: 80,
+          branches: 80,
+          functions: 80,
+          lines: 80
+        }
+      }
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
